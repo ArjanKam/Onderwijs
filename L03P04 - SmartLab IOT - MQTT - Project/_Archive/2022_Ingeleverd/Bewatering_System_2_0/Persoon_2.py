989aaab53d6e3f384e24dbebf61d8de334922685
Arjan <akamberg@davinci.nl>
2023-03-23 22:00:04 +0100

    reorganisatie

---
+++ L03P04 - SmartLab IOT - MQTT - Project/_Archive/2022_Ingeleverd/Bewatering_System_2_0/Persoon_2.py
@@ -0,0 +1,42 @@
+import paho.mqtt.client as mqtt
+import time
+
+# static values
+TOPIC = "G7/MOISTURE"
+TOPIC2 = "G7/VALVE"
+
+
+# Check the moisture level and send the data with MQTT to the wemos
+def on_message(client, userdata, message):
+    # Split the topic
+    topic = message.topic.split('/')
+    # Decode the moisture value
+    moisture = float(message.payload.decode("utf-8"))
+    # Check the moisture values
+    if (moisture <= 50):
+        valve = 1
+    if (moisture > 50):
+        valve = 0
+
+    # Send the valve value to the mqtt system
+    client.publish(TOPIC2, valve)
+
+    # Console print system that allow you to check the values
+    print(valve)
+    print(f"{topic} : {moisture}")
+
+# Connect to the MQTT client
+client = mqtt.Client("computer")
+client.connect("192.168.1.99")
+
+# Create a mqtt loop
+client.loop_start()
+
+# Subscribe to MQTT client and get the Moisture value
+client.subscribe(TOPIC)
+client.on_message = on_message
+
+# A loop that allows us to wait a bit before running again
+while True:
+    # pass # do not use pass... too much CPU consumption
+    time.sleep(10)
