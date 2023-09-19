4315f412176556e44cb20ee40d7f500d95eda1b3
Arjan <111748588+ArjanKam@users.noreply.github.com>
2023-09-19 16:07:19 +0200

    update

---
+++ L02P03 - Smartlab IOT - Python 3 - micropython/L02P03 - Week 4 - Schakelaars/Knop.py
@@ -0,0 +1,16 @@
+# demo voor gebruik van Knop
+from machine import Pin
+
+PIN_KNOP = 16 # Aansluiting op D0
+LED_D4   = 2  # Also Onboard LED
+
+knop = Pin( PIN_KNOP, Pin.IN)
+led4 = Pin( LED_D4, Pin.OUT)
+
+while True:
+    if knop.value() == True:
+        print("Hoog")
+        led.On()
+    else:
+        led.Off()
+    
