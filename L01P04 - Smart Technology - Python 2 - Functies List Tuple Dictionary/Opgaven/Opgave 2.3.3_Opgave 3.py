6b9e92b2cab02e0d63b79f2f231d63a45cce9dcc
Arjan <111748588+ArjanKam@users.noreply.github.com>
2023-11-16 19:48:40 +0100

    opgaves

---
+++ L01P04 - Smart Technology - Python 2 - Functies List Tuple Dictionary/Opgaven/Opgave 2.3.3_Opgave 3.py
@@ -0,0 +1,28 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 3')
+
+_currentPos = 0
+def move(pos):
+    global _currentPos
+    while _currentPos < pos:
+        robotArm.moveRight()
+        _currentPos += 1
+    while _currentPos > pos:
+        robotArm.moveLeft()
+        _currentPos -= 1
+    
+def moveTo(start, end):
+    move(start)
+    robotArm.grab()
+    move(end)
+    robotArm.drop()
+    
+# Jouw python instructies zet je vanaf hier:
+blocks = 4
+while blocks > 0:
+    moveTo(0,1)
+    blocks -= 1
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
