b6605caa93c718367c2bf60ec0ff90db5d67094c
Arjan <111748588+ArjanKam@users.noreply.github.com>
2024-01-23 14:52:13 +0100

    Added documents

--- L02P04 - Smart Technology - microPython 2 - Sensoren/Opgave 03 - Afstand meten/Libraries/pyGame_radar.py
+++ L02P04 - Smart Technology - microPython 2 - Sensoren/Opgave 03 - Afstand meten/Libraries/pyGame_radar.py
@@ -1,102 +1,115 @@
 import pygame
 import math
 import random
-#import measureUltrasoon as ultrasoon
 
-# Schermgrootte
+# import measureUltrasoon as ultrasoon
+
+# screen size
 WIDTH, HEIGHT = 800, 600
-# Radarinstellingen
+# Radar settings
 CENTER_X = WIDTH // 2
 CENTER_Y = HEIGHT
 DISTANCE_MAX = WIDTH // 2
 
-# Kleuren
+# colours
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
-GREEN = (0,255,0)
-DARKGREEN = (50,100,50)
+GREEN = (0, 255, 0)
+DARKGREEN = (50, 100, 50)
 
-BACKGROUND   = DARKGREEN
-SCANNER      = RED
+BACKGROUND = DARKGREEN
+SCANNER = RED
 COLOR_OBJECT = WHITE
 
-# Initialiseren van Pygame
+_angle = 0
+
+# Initialise Pygame
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Ultrasoon scanner")
 
-_angle = 0
-def measure():
+
+def readDistance():
     global _angle
-    #hoek, afstand = ultrasoon.readDistance()
-    hoek, afstand = (_angle, random.randint(5, 4* DISTANCE_MAX))
     _angle += 5
     if _angle > 360:
         _angle = 0
-    print(hoek, afstand)
-    return hoek, afstand
+    return _angle, random.randint(5, 4 * DISTANCE_MAX)
 
-def getX(angle, distance):
+
+def measure():
+    angle, distance = readDistance()
+    print(angle, distance)
+    return angle, distance
+
+
+def get_x(angle, distance):
     return int(distance * math.cos(math.radians(angle)))
 
-def getY(angle, distance):
+
+def get_y(angle, distance):
     return int(distance * math.sin(math.radians(angle)))  # Inverted Y-axis for correct display
 
-def getCoordinate(angle, distance):
-    x = CENTER_X + getX(angle, distance) 
-    y = CENTER_Y - getY(angle, distance)
-    return x,y  
-        
+
+def get_coordinate(angle, distance):
+    x = CENTER_X + get_x(angle, distance)
+    y = CENTER_Y - get_y(angle, distance)
+    return x, y
+
+
 # Functie om te controleren of objecten zich binnen het radarbereik bevinden
 _objects = {}
-_lastAngle = 0
+_lastAngle_draw = 0
+_oldAngle = 0
+
+
 def check_objects():
-    global _lastAngle
+    global _lastAngle_draw
     angle, distance = measure()
-    
+
     angle2 = angle
     if angle2 > 180:
-        angle2 = 360-angle2
+        angle2 = 360 - angle2
     if distance <= DISTANCE_MAX:
         _objects[angle2] = distance
     elif angle2 in _objects:
         del _objects[angle2]
-    
-    deltaAngle = abs(_lastAngle - angle2)
-    _lastAngle = angle2
-    
-    return angle, deltaAngle
 
-def drawArcLine(point_center, angle, step, distance, color ):    
-    point_1 = getCoordinate(angle - step/2, distance)
-    point_2 = getCoordinate(angle + step/2, distance)
+    delta_angle = abs(_lastAngle_draw - angle2)
+    _lastAngle_draw = angle2
+
+    return angle, delta_angle
+
+
+def draw_arc_line(point_center, angle, step, distance, color):
+    point_1 = get_coordinate(angle - step / 2, distance)
+    point_2 = get_coordinate(angle + step / 2, distance)
     pygame.draw.polygon(screen, color, (point_center, point_1, point_2))
-    
-_oldAngle = 0
-def drawScanLine(angle, step):
+
+
+def draw_scan_line(angle, step):
     global _oldAngle
     if angle > 180:
         angle = 360 - angle
-        
+
     point_center = (CENTER_X, CENTER_Y)
-    drawArcLine(point_center, _oldAngle, step, DISTANCE_MAX, BACKGROUND)
-    drawArcLine(point_center,     angle, step, DISTANCE_MAX, SCANNER)
-    
+    draw_arc_line(point_center, _oldAngle, step, DISTANCE_MAX, BACKGROUND)
+    draw_arc_line(point_center, angle, step, DISTANCE_MAX, SCANNER)
+
     _oldAngle = angle
 
-def drawObjects(color, step):
+
+def draw_objects(color, step):
     for angle in _objects.keys():
         distance = _objects[angle]
-        point_start = getCoordinate(angle, distance)
-        point_end   = getCoordinate(angle, DISTANCE_MAX)
+        point_start = get_coordinate(angle, distance)
         pygame.draw.circle(screen, RED, point_start, 1)
-        #pygame.draw.line  (screen, color, point_start, point_end)
-        drawArcLine(point_start, angle, step, DISTANCE_MAX, color)
-    for radius in range(0, HEIGHT, 50):
+        draw_arc_line(point_start, angle, step, DISTANCE_MAX, color)
+    for radius in range(0, HEIGHT, 50):  # draw line each 50 pixels
         pygame.draw.circle(screen, GREEN, (CENTER_X, CENTER_Y), radius, 1)
-    
-# Hoofdprogramma
+
+
 def main():
     running = True
     clock = pygame.time.Clock()
@@ -107,16 +120,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
         angle, step = check_objects()
-        
-        drawScanLine(angle, step) # Teken lijn van de scankop    
-        drawObjects(COLOR_OBJECT, step)
-
+        draw_scan_line(angle, step)  # draw line
+        draw_objects(COLOR_OBJECT, step)
         pygame.display.flip()
         clock.tick(30)
-        
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
